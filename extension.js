d7dca7043caad03d4c24af1f6ad581d98a0a5447
Sergey Shlyapugin <shlyapugin@gmail.com>
2025-06-03 00:48:08 +0300

    refactor: sorting imports

--- extension.js
+++ extension.js
@@ -1,12 +1,15 @@
-import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
+import St from 'gi://St';
+
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+
 import {panel} from 'resource:///org/gnome/shell/ui/main.js';
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 const LAYOUT_FLAGS = {
     'us': '🇺🇸',
